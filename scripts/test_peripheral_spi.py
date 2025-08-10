5e0a8b3cbc813538ddaad5e9ea06a7583829c22b
Jon Nordby <jononor@gmail.com>
2025-08-10 23:48:56 +0200

    scripts: Read PCM with interrupts and ringbuffer
    With RPI at 200 Mhz, it looks like we can read 10-11khz rate
    However reading gets stuck after some 10-40 samples,
    the interrupt stops triggering - possibly because a read was missed
    Doing a read again unblocks the situation (for a short while)

--- scripts/test_peripheral_spi.py
+++ scripts/test_peripheral_spi.py
@@ -12,105 +12,9 @@ REG_CLKP = 0x04
 REG_PCMW = 0x08
 
 
-def run_test():
-
-    # Set clock frequency of RP2040
-    machine.freq(133_000_000)
-
-    # Set everything to inputs, to be safe
-    for i in range(30):
-        Pin(i, Pin.IN, pull=None)
-
-    clk = start_peripheral()
-    print(clk)
-
-    # The SPI communication expects pico-ice
-
-    interrupt = Pin(1, Pin.IN)
-    
-    for i in range(10):
-        print(interrupt.value())
-        time.sleep(0.10)
-
-
-    # SPI communication
-    # Chip select, active low
-    spi_cs_n = Pin(13, Pin.OUT) # Pin 9 is used for ICE40 flash SS, should be avoided
-
-    # NOTE: Above 6 Mhz started to get read/write errors
-    spi = SPI(0, 6_000_000,
-            sck=Pin(2),
-            mosi=Pin(3),
-            miso=Pin(0),
-            #sck=Pin(10),
-            #mosi=Pin(11),
-            #miso=Pin(8),
-            bits=8,
-            phase=0,
-            polarity=0,
-            firstbit=SPI.MSB)
-    peri = PeripheralCommunicationSPI(spi, spi_cs_n)
-
-
-    ctrl = peri.read32(REG_CTRL)
-    print('control', ctrl)
-
-    # Set and check scale
-    clkp = peri.read32(REG_CLKP)
-    scale_config = bytearray([0x00, 0x00, 0x00, 64])
-    peri.write32(REG_CLKP, scale_config)
-
-    clkp = peri.read32(REG_CLKP)
-    assert clkp == scale_config, (clkp, scale_config)
-
-    #for i in range(10):
-    #    peri.write32(REG_CTRL, bytearray([0x00, 0x00, 0x00, 0x01]))
-    #    time.sleep(2.0)
-    #    peri.write32(REG_CTRL, bytearray([0x00, 0x00, 0x00, 0x00]))
-    #    time.sleep(2.0)
-
-    # Enable the PDM clock
-    print('enable clock')
-    clock_config = bytearray([0x00, 0x00, 0x00, 0x01])
-    peri.write32(REG_CTRL, clock_config)
-    ctrl = peri.read32(REG_CTRL)
-    assert ctrl == clock_config, (ctrl, clock_config)
-
-
-    print("PCM:")
-    pcm_start = time.ticks_us()
-    n_samples = 100
-    samples = bytearray(n_samples)
-    read_pcm_samples(peri, samples)
-    pcm_read_duration = time.ticks_diff(time.ticks_us(), pcm_start)
-    per_sample = pcm_read_duration / n_samples
-    deadline = 62 # at 16khz, only have 62 us between each sample
-    print('PCM read duration', per_sample)
-    assert per_sample < deadline, (per_sample, deadline)
-
-    print(samples)
-
-@micropython.native
-def read_pcm_samples(peri, samples):
-
-    n_samples = len(samples) # 1 byte per each, for now
-
-    # pre-allocate buffers
-    mem = memoryview(samples) 
-    cmd = bytearray([ 0b01000000, 0, 0, REG_PCMW ])
-    pcm = bytearray(4)
-
-    # cache member references, avoids lookup
-    # and we do not have the function call overhead
-    cs_n = peri.cs_n
-    spi = peri.spi
-
-    for i in range(n_samples):
-        cs_n.value(0)
-        spi.write(cmd)
-        spi.readinto(pcm)
-        mem[i] = pcm[3]
-        cs_n.value(1)
+# Setup buffer for error reports in interrupts
+import micropython
+micropython.alloc_emergency_exception_buf(100)
 
 class PeripheralCommunicationSPI():
 
@@ -165,6 +69,200 @@ class PeripheralCommunicationSPI():
 
         self.spi.write(p)
         self.cs_n.value(1)
+
+
+# globals
+spi = None
+spi_cs_n = None
+read_pcm_cmd = bytearray([ 0b01000000, 0, 0, REG_PCMW ])
+pcm_sample = bytearray(4)
+last_trigger : int = -1
+pcm_buffer = micropython.RingIO(4*16*10)
+
+timing_buffer = micropython.RingIO(4*16*10)
+timing_sample = bytearray(1)
+
+@micropython.native
+def pcm_ready_interrupt(p):
+    
+    # check how often we are able to go back to this
+    #global last_trigger
+    #t : int = time.ticks_us()
+    #if last_trigger > 0:
+    #    diff : int = time.ticks_diff(t, last_trigger)
+    #    timing_sample[0] = diff//10
+    #    timing_buffer.write(timing_sample)
+    #    #print('pcm trigg', diff)
+    #last_trigger = t
+
+    # Track how long we spend inside handler
+    #start = time.ticks_us()
+
+    #time.sleep_ms(1)
+    spi_cs_n.value(0)
+    spi.write(read_pcm_cmd)
+    spi.readinto(pcm_sample)
+    spi_cs_n.value(1)
+
+    # put pcm_sample onto queue
+    pcm_buffer.write(pcm_sample)
+
+    #duration = time.ticks_diff(time.ticks_us(), start)
+    #print('d', duration)
+
+def read_with_interrupt(duration=10):
+
+    total_samples = 0
+    read_buffer = bytearray(10)
+    pcm_start = time.ticks_us()
+    pcm_last = pcm_start
+    while True:
+
+        # check timing queue
+        #n_bytes = timing_buffer.readinto(read_buffer)
+        #if n_bytes > 0:
+        #    for t in read_buffer[0:n_bytes]:
+        #        print('t', t)
+
+        # check PCM queue
+        n_bytes = pcm_buffer.readinto(read_buffer)
+        if n_bytes > 0:
+            total_samples += (n_bytes/4)
+            pcm_last = time.ticks_us()
+
+        # check if we are stuck
+        since_last = time.ticks_diff(time.ticks_us(), pcm_last)
+        if since_last > 1000*1000:
+            print('TIMEOUT', since_last)
+            break
+
+        # check if we are done
+        since_start = time.ticks_diff(time.ticks_us(), pcm_start)
+        if since_start > int(duration*1000*1000):
+            print('FINISHED')
+            break
+
+        #time.sleep_ms()
+    
+    if total_samples == 0:
+        print('FAILED, 0 samples')
+    delta = time.ticks_diff(pcm_last, pcm_start)
+    effective_samplerate = total_samples / (delta/1e6)
+    print('read', delta/1e3, total_samples, effective_samplerate)
+
+def run_test():
+
+    # Set clock frequency of RP2040
+    machine.freq(200_000_000)
+
+    # Set everything to inputs, to be safe
+    for i in range(30):
+        Pin(i, Pin.IN, pull=None)
+
+    clk = start_peripheral()
+    print(clk)
+
+    # The SPI communication expects pico-ice
+    interrupt = Pin(1, Pin.IN)
+    
+    for i in range(10):
+        print(interrupt.value())
+        time.sleep(0.10)
+
+    interrupt.irq(trigger=Pin.IRQ_RISING, handler=pcm_ready_interrupt)
+
+    # SPI communication
+    # Chip select, active low
+    global spi_cs_n
+    spi_cs_n = Pin(13, Pin.OUT) # Pin 9 is used for ICE40 flash SS, should be avoided
+
+    # NOTE: Above 6 Mhz started to get read/write errors
+    global spi
+    spi = SPI(0, 6_000_000,
+            sck=Pin(2),
+            mosi=Pin(3),
+            miso=Pin(0),
+            #sck=Pin(10),
+            #mosi=Pin(11),
+            #miso=Pin(8),
+            bits=8,
+            phase=0,
+            polarity=0,
+            firstbit=SPI.MSB)
+    peri = PeripheralCommunicationSPI(spi, spi_cs_n)
+
+
+    ctrl = peri.read32(REG_CTRL)
+    print('control', ctrl)
+
+    # Set and check scale
+    clkp = peri.read32(REG_CLKP)
+    scale_config = bytearray([0x00, 0x00, 0x00, 64])
+    peri.write32(REG_CLKP, scale_config)
+
+    clkp = peri.read32(REG_CLKP)
+    assert clkp == scale_config, (clkp, scale_config)
+
+    #for i in range(10):
+    #    peri.write32(REG_CTRL, bytearray([0x00, 0x00, 0x00, 0x01]))
+    #    time.sleep(2.0)
+    #    peri.write32(REG_CTRL, bytearray([0x00, 0x00, 0x00, 0x00]))
+    #    time.sleep(2.0)
+
+    # Enable the PDM clock
+    print('enable clock')
+    clock_config = bytearray([0x00, 0x00, 0x00, 0x01])
+    peri.write32(REG_CTRL, clock_config)
+    ctrl = peri.read32(REG_CTRL)
+    assert ctrl == clock_config, (ctrl, clock_config)
+
+    # Try to read PCM data as fast as we can
+    for i in range(10):
+        read_with_interrupt()
+
+        # HACK: try to recover missing interrupt by doing a read
+        peri.read32(REG_PCMW)
+
+    ### XXX:
+    return
+
+    print("PCM:")
+    pcm_start = time.ticks_us()
+    n_samples = 600
+    samples = bytearray(n_samples)
+    read_pcm_samples_loop(peri, samples)
+    pcm_read_duration = time.ticks_diff(time.ticks_us(), pcm_start)
+    per_sample = pcm_read_duration / n_samples
+    deadline = 62 # at 16khz, only have 62 us between each sample
+    print('PCM read duration', per_sample)
+    assert per_sample < deadline, (per_sample, deadline)
+
+    print(samples)
+
+@micropython.native
+def read_pcm_samples_loop(peri, samples):
+
+    n_samples = len(samples) # 1 byte per each, for now
+
+    # pre-allocate buffers
+    mem = memoryview(samples) 
+    cmd = bytearray([ 0b01000000, 0, 0, REG_PCMW ])
+    pcm = bytearray(4)
+
+    # cache member references, avoids lookup
+    # and we do not have the function call overhead
+    cs_n = peri.cs_n
+    spi = peri.spi
+
+    for i in range(n_samples):
+        cs_n.value(0)
+        spi.write(cmd)
+        spi.readinto(pcm)
+        #print(pcm)
+        mem[i] = pcm[3]
+        cs_n.value(1)
+
+
 
 def start_peripheral():
 
